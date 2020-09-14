936134806e9f763335c22eb525eb4a4bbd36a8ce
程金盛 <42282367@qq.com>
2020-09-14 21:55:43 +0800

    edit func

--- src/Helper.php
+++ src/Helper.php
@@ -2,5 +2,22 @@
 namespace CjsProtocol;
 
 function toJson($ary) {
-    return json_encode($ary, JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_UNICODE);
+    return \json_encode($ary, JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_UNICODE);
+}
+
+function json_encode($data, $options=null) {
+    if(is_null($options)) {
+        $options = 0;
+        if (defined('JSON_UNESCAPED_SLASHES')) {
+            $options |= JSON_UNESCAPED_SLASHES;
+        }
+        if (defined('JSON_UNESCAPED_UNICODE')) {
+            $options |= JSON_UNESCAPED_UNICODE;
+        }
+    }
+    return \json_encode($data, $options);
+}
+
+function with($object) {
+    return $object;
 }
